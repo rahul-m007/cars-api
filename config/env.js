3549f00acf40e4bd87d33732b0466772ad6eb596
Rahul Martin <rahulmartin007@gmail.com>
2022-03-17 15:45:51 +1300

    Add db configurations and sequelize connection

---
+++ config/env.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const env = {
+    database: process.env.DATABASE,
+    username: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+  
+module.exports = env;
