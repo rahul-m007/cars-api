30fe9673292b83567a9d5873c75dcaf2d234464b
Rahul Martin <rahulmartin007@gmail.com>
2022-03-17 16:15:14 +1300

    Add db and routes to the server

--- model/car.model.js
+++ model/car.model.js
@@ -4,19 +4,31 @@ module.exports = (sequelize, DataTypes) => {
         brand       : {
             type        : DataTypes.STRING,
             allowNull   : false,
+            validate: {
+                notNull: { msg: "Brand is required" },
+              },
         },
         model       : {
             type        : DataTypes.STRING,
             allowNull   : false,
+            validate: {
+                notNull: { msg: "Model is required" },
+            },
         },
         engineType  : {
             type        : DataTypes.ENUM,
             values      : ['electric', 'petrol', 'diesel'],
             allowNull   : false,
+            validate: {
+                notNull: { msg: "Engine Type is required" },
+            },
           },
         year        : {
             type        : DataTypes.INTEGER,
             allowNull   : false,
+            validate: {
+                notNull: { msg: "Year is required" },
+            },
         }
     })
 
