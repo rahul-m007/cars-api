d75c4bf5b25447c4f46c814056851df7de5ce929
Rahul Martin <rahulmartin007@gmail.com>
2022-03-17 16:12:09 +1300

    Add db and routes to the server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express       = require("express");
 const cors          = require("cors");
 const bodyParser    = require('body-parser');
+const db            = require('./config/database');
+const routes        = require('./api');
 
 const app = express();
  
@@ -8,10 +10,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
  
+
+//force: true will drop the table if it already exists
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync with { force: true }');
+});
+ 
 // api routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to CAR API App." });
 });
+
+app.use('/api', routes());
  
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
